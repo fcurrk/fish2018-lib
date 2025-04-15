3b7f9c9731cbc8442906267f166311082657da43
fcurrk <48197714+fcurrk@users.noreply.github.com>
2025-04-15 12:34:19 +0800

    Update py_偷乐短剧.py

--- py/py_偷乐短剧.py
+++ py/py_偷乐短剧.py
@@ -281,7 +281,7 @@ class Spider(Spider):
         
         # 处理页码
         if pg is None:
-            pg = 2
+            pg = 1
         else:
             pg = int(pg)
 
@@ -396,7 +396,7 @@ class Spider(Spider):
                 # 查找分页信息
                 # 默认值
                 total = len(videos)
-                pagecount = 1
+                pagecount = 10
                 limit = 20
                 
                 # 尝试查找分页元素
