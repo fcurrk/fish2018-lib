b84b7c9c95879a9709145bc615971cee928017a8
fcurrk <48197714+fcurrk@users.noreply.github.com>
2026-09-12 09:06:20 +0800

    Update py_huangguo.py

--- py/py_huangguo.py
+++ py/py_huangguo.py
@@ -27,7 +27,7 @@ class Spider(Spider):
         return "黄果短剧"
 
     def init(self, extend=""):
-        self.host = "https://huangguoai.com"
+        self.host = "https://huangguo.video"
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
